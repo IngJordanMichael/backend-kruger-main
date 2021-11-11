44a1e6c9013b51311852faa7021a52eb19c407d6
Jhonny Javier Muñoz Cedeño <jmunoz@ec.krugercorp.com>
2021-11-10 19:21:45 -0500

    Add Enviroment

--- src/app/services/crud-pokemons.service.ts
+++ src/app/services/crud-pokemons.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Pokemon } from '../Models/pokemon';
 
 @Injectable({
@@ -8,39 +9,38 @@ import { Pokemon } from '../Models/pokemon';
 })
 export class CrudPokemonsService {
 
-  urlBase : string = 'https://pokemon-pichincha.herokuapp.com/pokemons/';
  
   constructor(private http:HttpClient) { }
 
   getPokemonById(id:number){
-    return this.http.get(this.urlBase + id).pipe(
+    return this.http.get(environment.apiUrl + id).pipe(
       map(data=>data)
     );
   }
 
   getRecordsByIdAuthor(id:number){
-    return this.http.get(this.urlBase + 'count?idAuthor=' + id).pipe(
+    return this.http.get(environment.apiUrl + 'count?idAuthor=' + id).pipe(
       map(data=>data)
     );
   }
 
   getAllPokemons(){
-    return this.http.get(this.urlBase).pipe(
+    return this.http.get(environment.apiUrl).pipe(
       map(data=>data))
   }
 
   createPokemon(pokemon:Pokemon){
     let headers = new HttpHeaders().set('Content-Type','application/json');
-    return this.http.post(this.urlBase + "?idAuthor=1",pokemon,{headers: headers});
+    return this.http.post(environment.apiUrl + "?idAuthor=1",pokemon,{headers: headers});
   }
 
   editPokemon(pokemon:Pokemon){
     let headers = new HttpHeaders().set('Content-Type','application/json');
-    return this.http.put(this.urlBase + pokemon.id,pokemon,{headers: headers});
+    return this.http.put(environment.apiUrl + pokemon.id,pokemon,{headers: headers});
   }
 
   deletePokemon(id:number){
-    return this.http.delete(this.urlBase + id);
+    return this.http.delete(environment.apiUrl + id);
   }
 
 }
